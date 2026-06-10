10be69a2709abd54e81f956086c9e5d77d7a30a5
muntashir-wahid <muntashir.wahid@yahoo.com>
2026-06-10 13:45:18 +0600

    added front end live url on cors config

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,9 @@ const app: Application = express();
 // Middleware
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    origin: ['http://localhost:5173', 'https://zudrop.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   }),
 );
 app.use(morgan('dev'));
