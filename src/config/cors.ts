2b1054cef75c33f461d99f4f6e1a1df382d328d3
muntashir-wahid <muntashir.wahid@yahoo.com>
2026-06-11 13:44:22 +0600

    updated cors for vite preview

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,1 +1,1 @@
-export const allowedOrigins = ['http://localhost:5173', 'https://zudrop.vercel.app'];
+export const allowedOrigins = ['http://localhost:5173', 'https://zudrop.vercel.app', 'http://localhost:4173'];
